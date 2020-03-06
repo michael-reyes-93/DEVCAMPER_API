bd59ff2ad31a7c3a3ca30abbc575de2249f8c0e9
Michael reyes <michaelreyesfernandez@hotmail.com>
2020-03-05 22:38:22 -0700

    nodemon configuration, morgan library, creation of the first middleware, applying morgan for dev enviroment only

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const logger = require('./middleware/logger')
+const morgan = require('morgan');
 
 // Route files
 const bootcamps = require('./routes/bootcamps');
@@ -9,12 +11,17 @@ dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
+// Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(
-    PORT,
-    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
