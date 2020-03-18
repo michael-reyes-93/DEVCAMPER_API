4ec6630dcf309ce5f5c31c704f0b75bf1a3c53f7
Michael reyes <michaelreyesfernandez@hotmail.com>
2020-03-18 01:58:22 -0700

    Auth protect middleware

--- controllers/auth.js
+++ controllers/auth.js
@@ -49,7 +49,6 @@ exports.login = asyncHandler(async (req, res, next) => {
   sendTokenResponse(user, 200, res);
 });
 
-
 // Get token from model, create cookie and send response
 const sendTokenResponse = (user, statusCode, res) => {
   // Create token
@@ -71,4 +70,16 @@ const sendTokenResponse = (user, statusCode, res) => {
       success:true,
       token
     });
-}
+};
+
+// @desc        Get current logged in user   
+// @route       POST /api/v1/auth/me 
+// @access      Private
+exports.getMe = asyncHandler(async (req, res, next) => {
+  const user = await User.findById(req.user.id);
+
+  res.status(200).json({
+    success: true,
+    data: user
+  });
+});
