a02392338c74f3353adf076b3453ae57838780cf
Michael reyes <michaelreyesfernandez@hotmail.com>
2020-03-17 21:32:46 -0700

    User register & encrypting passwords

---
+++ models/User.js
@@ -0,0 +1,34 @@
+const mongooose = require('mongoose');
+
+const UserSchema = new mongooose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please add a name']
+  },
+  email: {
+    type: String,
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      'Please add a valid email'
+    ]
+  },
+  role: {
+    type: String,
+    enum: ['user', 'publisher'],
+    default: 'user'
+  },
+  password: {
+    type: String,
+    required: [true, 'Please add a password'],
+    minlength: 6,
+    select: false
+  },
+  resetPasswordToken: String,
+  resetPasswordExpire: Date,
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongooose.model('User', UserSchema);
